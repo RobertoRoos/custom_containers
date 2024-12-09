2d3f3ddfc5b0a151d75891cdf6428f9556b3f5da
RobertoRoos <robert.soor@gmail.com>
2024-12-09 18:58:26 +0100

    Progressing with fifo

--- include/cc/fifo.hxx
+++ include/cc/fifo.hxx
@@ -59,12 +59,12 @@ public:
 	/**
 	 * Return the number of elements still unused and available.
 	 */
-	std::size_t free() const
+	std::size_t free() const noexcept
 	{
 		return this->max_size() - size();
 	}
 
-	bool full() const
+	bool full() const noexcept
 	{
 		return m_head == m_tail + this->max_size();
 	}
@@ -81,8 +81,7 @@ public:
 		if(full()) // No space left, don't quitely overwrite
 			std::__throw_out_of_range("fifo::push");
 
-		const std::size_t head_real = m_head % this->max_size();
-		this->operator[](head_real) = v;
+		this->operator[](head_modulo()) = v;
 		// `m_head` can actually exceed max_size
 		m_head++;
 		// Don't modulo, only do that on tail updates
@@ -94,24 +93,79 @@ public:
 			std::__throw_out_of_range("fifo::pop"); // No items left
 
 		const auto v = this->operator[](m_tail);
-		m_tail++;
-		if(m_tail >= this->max_size()) {
-			m_tail = 0;
-			m_head -= this->max_size();
-			// Update both, instead of each modulo-ing each time
-		}
+		increment_tail();
 		return v;
 	}
 
-	template <typename other_iterator>
-	void push_list(other_iterator other_begin, other_iterator other_end)
+	void push_list(const value_type values[], std::size_t elements)
 	{
-		;
+		push_list(values, values + elements);
+	}
+
+	void push_list(const value_type* other_begin, const value_type* other_end)
+	{
+		const std::size_t n = other_end - other_begin;
+		if(free() < n)
+			std::__throw_out_of_range("fifo::push_list"); // Not enough space left
+
+		// Copy elements until the end of the buffer:
+		const std::size_t n1 = std::min(n, this->max_size() - head_modulo());
+		std::copy(other_begin, other_begin + n1, this->data() + head_modulo());
+		// Copy elements from the start of the buffer:
+		const std::size_t n2 = n - n1;
+		std::copy(other_begin + n1, other_begin + n, this->data());
+		m_head += n; // Don't modulo, do that in pop_*
+	}
+
+	/**
+	 * Remove multiple elements from the queue.
+	 *
+	 * @param n Number of items - Default: take all available items
+	 */
+	void pop_list(value_type* other_begin, std::size_t n = 0)
+	{
+		if(n > 0 && n > size())
+			std::__throw_out_of_range("fifo::pop_list"); // Not enough items left
+		else if(n == 0)
+			n = size();
+
+		// Copy elements until the end of the buffer:
+		const std::size_t n1 = std::min(n, this->max_size() - m_tail);
+		std::copy(this->data() + m_tail, this->data() + m_tail + n1, other_begin);
+		// Copy elements from the start of the buffer:
+		const std::size_t n2 = n - n1;
+		std::copy(this->data(), this->data() + n2, other_begin + n1);
+		increment_tail(n);
 	}
 
 	/* }@ */
 
+	iterator begin() noexcept
+	{
+		return iterator(this->data() + head_modulo());
+	}
+
+	iterator end() noexcept
+	{
+		return iterator(this->data() + m_tail);
+	}
+
 protected:
+	void increment_tail(std::size_t incr = 1)
+	{
+		m_tail += incr;
+		if(m_tail >= this->max_size()) {
+			m_tail -= this->max_size();
+			m_head -= this->max_size();
+			// Update both, instead of each modulo-ing each time
+		}
+	}
+
+	std::size_t head_modulo() const
+	{
+		return m_head % this->max_size(); // Head can exceed max_size, return modulo
+	}
+
 	std::size_t m_tail; // Index of the next value to read
 	std::size_t m_head; // Index of the next value to write
 			    // Tail-to-head is the section with valid data
--- tests/test_fifo.cpp
+++ tests/test_fifo.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <vector>
+
 #include "cc/fifo.hxx"
 
 TEST(FifoTest, Empty)
@@ -61,11 +63,69 @@ TEST(FifoTest, PushPop)
 	ASSERT_TRUE(data.empty());
 }
 
-TEST(FifoTest, AddList)
+TEST(FifoTest, PushList)
 {
-	cc::fifo<float, 10> data;
+	cc::fifo<float, 5> data;
 
 	std::array<float, 3> src{1.0f, 2.0f, 3.0f};
 
 	data.push_list(src.begin(), src.end());
+
+	ASSERT_EQ(data.size(), 3);
+	ASSERT_EQ(data.pop(), 1.0f);
+	ASSERT_EQ(data.pop(), 2.0f);
+	ASSERT_EQ(data.pop(), 3.0f);
+	ASSERT_TRUE(data.empty());
+
+	data.push_list(src.begin(), src.end());
+
+	ASSERT_EQ(data.size(), 3);
+	ASSERT_EQ(data.pop(), 1.0f);
+	ASSERT_EQ(data.pop(), 2.0f);
+	ASSERT_EQ(data.pop(), 3.0f);
+	ASSERT_TRUE(data.empty());
+}
+
+TEST(FifoTest, PopList)
+{
+	cc::fifo<float, 5> data;
+
+	data.push(1.0f);
+	data.push(2.0f);
+	data.push(3.0f);
+	ASSERT_EQ(data.size(), 3);
+
+	std::array<float, 3> dst{};
+	data.pop_list(dst.begin());
+	ASSERT_EQ(dst[0], 1.0f);
+	ASSERT_EQ(dst[1], 2.0f);
+	ASSERT_EQ(dst[2], 3.0f);
+	ASSERT_EQ(data.size(), 0);
+
+	data.push(1.0f);
+	data.push(2.0f);
+	data.push(3.0f);
+	ASSERT_EQ(data.size(), 3);
+
+	dst.fill(0.0f);
+	data.pop_list(dst.begin(), 3);
+	ASSERT_EQ(dst[0], 1.0f);
+	ASSERT_EQ(dst[1], 2.0f);
+	ASSERT_EQ(dst[2], 3.0f);
+	ASSERT_EQ(data.size(), 0);
+}
+
+
+TEST(FifoTest, ForLoop)
+{
+	cc::fifo<float, 5> data;
+	data.push(1.0f);
+	data.push(2.0f);
+	data.push(3.0f);
+
+	float check = 1.0f;
+	for(const auto& v : data) {
+		ASSERT_EQ(check, v);
+		check += 1.0f;
+	}
 }
